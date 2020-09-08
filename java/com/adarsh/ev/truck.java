d74f66bd255b464081ac8567464167b68ab2dbc1
Adarsh Vishwakarma <64848697+adarshvishwakarma@users.noreply.github.com>
2020-09-08 22:12:34 +0530

    Add files via upload

---
+++ java/com/adarsh/ev/truck.java
@@ -0,0 +1,14 @@
+package com.adarsh.ev;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class truck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_truck);
+    }
+}
